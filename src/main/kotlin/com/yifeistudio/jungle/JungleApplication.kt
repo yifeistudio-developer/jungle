f6c1ae2ed71ebf0a4ab065729d6c0fb0ae837c4b
hongyi <develop@yifeistudio.com>
2024-03-15 18:49:38 +0800

    first commit

---
+++ src/main/kotlin/com/yifeistudio/jungle/JungleApplication.kt
@@ -0,0 +1,11 @@
+package com.yifeistudio.jungle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JungleApplication
+
+fun main(args: Array<String>) {
+	runApplication<JungleApplication>(*args)
+}
---
+++ src/test/kotlin/com/yifeistudio/jungle/JungleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yifeistudio.jungle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JungleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
