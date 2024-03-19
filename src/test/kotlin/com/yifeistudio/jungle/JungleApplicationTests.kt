84f888208f12f46a41b7fbce3cd9a4bcd1652e9f
hongyi <develop@yifeistudio.com>
2024-03-19 10:18:13 +0800

    调整消息关系名称

--- src/test/kotlin/com/yifeistudio/jungle/JungleApplicationTests.kt
+++ src/test/kotlin/com/yifeistudio/jungle/JungleApplicationTests.kt
@@ -3,7 +3,6 @@ package com.yifeistudio.jungle
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient
-import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.net.URI
 
@@ -18,12 +17,12 @@ class JungleApplicationTests {
 	fun websocketUserEndpointTest() {
 		val client = ReactorNettyWebSocketClient()
 		val url = URI("ws://localhost:8080/user-endpoint/message")
-		client.execute(url) { session ->
-			val output = session.send(Flux.empty<String>().map(session::textMessage))
-				.repeat(3).then()
-			val then = session.receive()
-				.doOnNext(::println).then()
-			Mono.zip(output, then).then()
+		client.execute(url) {
+			session ->
+			session.send(Mono.just(session.textMessage("hello world")))
+				.thenMany(session.receive())
+				.doOnNext { message -> println(message.payloadAsText) }
+				.then()
 		}.block()
 	}
 
